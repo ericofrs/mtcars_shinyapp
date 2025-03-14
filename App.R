1e614063009b1e092982c9f79d752044151eca15
Ibtissam <ibtissam.elomari@student.dlh.lu>
2025-03-14 09:43:18 +0000

    creation the stucture of the app and the ui

---
+++ App.R
@@ -0,0 +1,37 @@
+library(shiny)
+library(plotly)
+library(reactable)
+library(DT)
+library(highcharter)
+
+#call modules
+
+ui <- fluidPage(
+  titlePanel("MTcars Shiny App"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("var", "Choose a variable:", choices = names(mtcars)[1:4]),
+      sliderInput("range", "Filter by mpg:", min = min(mtcars$mpg), max = max(mtcars$mpg), value = c(min(mtcars$mpg), max(mtcars$mpg))),
+      actionButton("apply_filter", "Apply Filters")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Plotly", plotlyOutput("plotly_plot")),
+        tabPanel("Reactable", reactableOutput("reactable_table")),
+        tabPanel("DT Table", DTOutput("dt_table")),
+        tabPanel("Highcharter", highchartOutput("highcharter_plot"))
+      )
+    )
+  )
+)
+
+server <- function(input, output, session) {
+
+
+  
+  # Call modules
+
+}
+
+shinyApp(ui, server)
